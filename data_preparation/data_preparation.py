136fdbf608eb1adb72d0ee484ad9319e6d5a9240
Tony Edgin <tedgin@cyverse.org>
2024-05-08 15:31:03 -0700

    DS-694: Addressed pylint issues

--- data_preparation/data_preparation.py
+++ data_preparation/data_preparation.py
@@ -1,3 +1,9 @@
+"""
+This script combines information from a fieldbook file with an entropy tar file, producing a unified
+JSON file for indexing a single scan date. To cover all scan dates, execute this script multiple
+times and changing the entropy_file_path argument.
+"""
+
 from os import path
 import sys
 import csv
@@ -23,10 +29,22 @@ def parse_fieldbook_csv_file(fieldbook_csv_path: str) -> dict:
     - fieldbook_csv_path (str): The file path to the CSV file to be parsed.
 
     Returns:
-    - dict: A dictionary with plant names as keys and a dictionary of their corresponding fieldbook data as values.
-    If a plant name is repeated, only the first occurrence is added to the dictionary.
-    e.g. {'year': 2019, 'experiment': 'lettuce_season_1', 'field': 'south', 'treatment': 'treat1',
-         'rep': '', 'range': 2, 'column': 4, 'plot': 204, 'type': 'border', 'genotype': 'Green_Towers_BORDER'}
+        A dictionary with plant names as keys and a dictionary of their corresponding fieldbook
+        data as values. If a plant name is repeated, only the first occurrence is added to the
+        dictionary. e.g.
+
+        {
+            'year': 2019,
+            'experiment': "lettuce_season_1",
+            'field': "south",
+            'treatment': "treat1",
+            'rep': "",
+            'range': 2,
+            'column': 4,
+            'plot': 204,
+            'type': "border",
+            'genotype': "Green_Towers_BORDER"
+        }
     """
     fieldbook_dict = {}
 
@@ -36,32 +54,34 @@ def parse_fieldbook_csv_file(fieldbook_csv_path: str) -> dict:
             next(reader, None)  # Skip header
             for row in reader:
                 try:
-                    year, experiment, field, treatment, rep, range_, column, plot, type_, genotype, plant_name = row
                     field_entry = {
-                        "year": int(year),
-                        "experiment": experiment,
-                        "field": field,
-                        "treatment": treatment,
-                        "rep": rep,
-                        "range": int(range_),
-                        "column": int(column),
-                        "plot": int(plot),
-                        "type": type_,
-                        "genotype": genotype
+                        "year": int(row[0]),
+                        "experiment": row[1],
+                        "field": row[2],
+                        "treatment": row[3],
+                        "rep": row[4],
+                        "range": int(row[4]),
+                        "column": int(row[6]),
+                        "plot": int(row[7]),
+                        "type": row[8],
+                        "genotype": row[9]
                     }
+                    plant_name = row[10]
                     if plant_name in fieldbook_dict:
-                        print(f"Unexpected - Duplicate plant name found: {plant_name}. Ignoring it.")
+                        print(
+                            f"Unexpected - Duplicate plant name found: {plant_name}. Ignoring it.")
                     else:
                         fieldbook_dict[plant_name] = field_entry
                 except ValueError as ve:
                     print(f"Issue with the number of columns or data conversion. Exiting!! \n{ve}")
-                    exit(1)
+                    sys.exit(1)
         return fieldbook_dict
 
 
-def download_and_extract_entropy_tar_file(irods_file_path: str, _IRODS_ENV_FILE: str) -> list[str]:
+def download_and_extract_entropy_tar_file(irods_file_path: str) -> list[str]:
     """
-    Downloads a tar file from an iRODS collection, extracts it, and returns a list containing the file names inside the tar file.
+    Downloads a tar file from an iRODS collection, extracts it, and returns a list containing the
+    file names inside the tar file.
 
     Parameters:
     - irods_file_path (str): The iRODS path to the tar file.
@@ -71,20 +91,26 @@ def download_and_extract_entropy_tar_file(irods_file_path: str, _IRODS_ENV_FILE:
     - file_names (list[string]): List containing the file names inside the tar file.
     """
     with iRODSSession(irods_env_file=_IRODS_ENV_FILE) as session:
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            local_tar_path = os.path.join(tmpdirname, "entropy_tar.tar")
+        with tempfile.TemporaryDirectory() as tmpdir_name:
+            local_tar_path = os.path.join(tmpdir_name, "entropy_tar.tar")
             obj = session.data_objects.get(irods_file_path)
+            replica = None
+            for r in obj.replicas:
+                if r.status == 1:
+                    replica = r
+                    break
             # Read data from the iRODS object into a buffer
-            data_buffer = bytearray(obj.size)
+            data_buffer = bytearray(replica.size)
             num_bytes_read = obj.open('r').readinto(data_buffer)
-            if num_bytes_read != obj.size:
+            if num_bytes_read != replica.size:
                 raise RuntimeError("Failed to read the entropy tar file, Exiting!!")
             # Write the buffer to the temporary file
             with open(local_tar_path, 'wb') as temp_file:
                 temp_file.write(data_buffer)
-            # Open the downloaded tar file, extract its contents, and list the names of the files inside
+            # Open the downloaded tar file, extract its contents, and list the names of the files
+            # inside
             with tarfile.open(local_tar_path) as tar:
-                tar.extractall(path=tmpdirname)
+                tar.extractall(path=tmpdir_name)
                 file_names = tar.getnames()  # List the file names after extracting
         return file_names
 
@@ -99,47 +125,57 @@ def parse_url_details(url: str) -> dict:
     Returns:
     - dict: A dictionary containing the extracted details.
     """
-    pattern = r"/season_([0-9]+)_([a-zA-Z]+)_yr_[0-9]+/level_([0-9]+)/([^/]+)/[^w]+?/([0-9]{4})-([0-9]{2})-([0-9]{2})__([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{3})"
+    pattern = (
+        r"/season_([0-9]+)_([a-zA-Z]+)_yr_[0-9]+" +
+        r"/level_([0-9]+)" +
+        r"/([^/]+)" +
+        r"/[^w]+?" +
+        r"/([0-9]{4})-([0-9]{2})-([0-9]{2})__([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{3})")
     match = re.search(pattern, url)
     if match:
-        season, crop_type, level, instrument, YYYY, MM, DD, hh, mm, ss, sss = match.groups()
+        season, crop_type, level, instrument, yyyy, mon, dd, hh, mm, ss, sss = match.groups()
         return {
             "season": int(season),
             "crop_type": crop_type,
             "level": int(level),
             "instrument": instrument,
-            "YYYY": YYYY,
-            "MM": MM,
-            "DD": DD,
+            "YYYY": yyyy,
+            "MM": mon,
+            "DD": dd,
             "hh": hh,
             "mm": mm,
             "ss": ss,
             "sss": sss
         }
 
-    pattern = r"/season_([0-9]+)_([a-zA-Z]+)_yr_[0-9]+/level_([0-9]+)/([^/]+)/[^w]+?/([0-9]{4})-([0-9]{2})-([0-9]{2})"
+    pattern = (
+        r"/season_([0-9]+)_([a-zA-Z]+)_yr_[0-9]+" +
+        r"/level_([0-9]+)" +
+        r"/([^/]+)" +
+        r"/[^w]+?" +
+        r"/([0-9]{4})-([0-9]{2})-([0-9]{2})")
     match = re.search(pattern, url)
     if match:
-        season, crop_type, level, instrument, YYYY, MM, DD = match.groups()
+        season, crop_type, level, instrument, yyyy, mon, dd = match.groups()
         return {
             "season": int(season),
             "crop_type": crop_type,
             "level": int(level),
             "instrument": instrument,
-            "YYYY": YYYY,
-            "MM": MM,
-            "DD": DD,
+            "YYYY": yyyy,
+            "MM": mon,
+            "DD": dd,
             "hh": "00",
             "mm": "00",
             "ss": "00",
             "sss": "000"
         }
-    else:
-        # Raise an error if the URL doesn't match the expected pattern
-        raise RuntimeError("Failed to parse URL. Exiting!!")
+
+    # Raise an error if the URL doesn't match the expected pattern
+    raise RuntimeError("Failed to parse URL. Exiting!!")
 
 
-def parse_entropy_tar_file(fieldbook_dict, csv_file_names, parsed_url):
+def _parse_entropy_tar_file(fieldbook_dict, csv_file_names, parsed_url):
     json_list = []
     for csv_file_name in csv_file_names:
         if not csv_file_name.endswith(".csv"):
@@ -147,22 +183,23 @@ def parse_entropy_tar_file(fieldbook_dict, csv_file_names, parsed_url):
         plant_name = csv_file_name.removesuffix("_volumes_entropy.csv")
         match = re.search(r'\/(.+?)_+[0-9]+$', plant_name)
         genotype = match.group(1) if match else plant_name
-        season = parsed_url["season"]
-        crop_type = parsed_url["crop_type"]
-        level = parsed_url["level"]
-        instrument = parsed_url["instrument"]
-        scan_date = parsed_url['YYYY'] + parsed_url['MM'] + parsed_url['DD'] + 'T' + parsed_url['hh'] + parsed_url['mm'] + parsed_url['ss'] + '.' + parsed_url['sss'] + '-0700'
         plant_name = plant_name.split("/")[1]
         if plant_name in fieldbook_dict:
+            scan_date = (
+                parsed_url['YYYY'] + parsed_url['MM'] + parsed_url['DD'] + 'T' +
+                parsed_url['hh'] +
+                parsed_url['mm'] +
+                parsed_url['ss'] + '.' + parsed_url['sss'] +
+                '-0700')
             fb_info = fieldbook_dict[plant_name]
             plant_dict = {
                 "plant_name": plant_name,
                 "genotype": genotype,
-                "season": season,
-                "crop_type": crop_type,
+                "season": parsed_url["season"],
+                "crop_type": parsed_url["crop_type"],
                 "year_of_planting": fb_info["year"],
-                "level": level,
-                "instrument": instrument,
+                "level": parsed_url["level"],
+                "instrument": parsed_url["instrument"],
                 "scan_date": scan_date,
                 "field": fb_info["field"],
                 "experiment": fb_info["experiment"],
@@ -175,14 +212,17 @@ def parse_entropy_tar_file(fieldbook_dict, csv_file_names, parsed_url):
             }
             json_list.append(plant_dict)
         else:
-            print(f"{plant_name} not found in fieldbook. Check fieldbook data or plant name. \nIgnoring {plant_name}")
+            print(f"{plant_name} not found in fieldbook. Check fieldbook data or plant name.")
+            print(f"Ignoring {plant_name}")
 
     # Create the output directory if it doesn't exist
     output_dir = "output"
     os.makedirs(output_dir, exist_ok=True)
 
     # Writing the combined information to a JSON file to be index ready by OpenSearch
-    with open(os.path.join(output_dir, f"combined_plants_info_{scan_date}.json"), "w") as json_file:
+    with open(
+        path.join(output_dir, f"combined_plants_info_{scan_date}.json"), 'w', encoding='utf-8'
+    ) as json_file:
         json.dump(json_list, json_file, indent=4)
 
 
@@ -200,19 +240,14 @@ def main(fieldbook_csv_path: str, entropy_file_path: str) -> None:
     fieldbook_dict = parse_fieldbook_csv_file(fieldbook_csv_path)
     print(fieldbook_dict)
     # Parse the entropy file
-    csv_file_names = download_and_extract_entropy_tar_file(entropy_file_path, _IRODS_ENV_FILE)
+    csv_file_names = download_and_extract_entropy_tar_file(entropy_file_path)
     # Parse the URL
     parsed_url = parse_url_details(entropy_file_path)
     # Combine everything above
-    parse_entropy_tar_file(fieldbook_dict, csv_file_names, parsed_url)
+    _parse_entropy_tar_file(fieldbook_dict, csv_file_names, parsed_url)
 
 
 if __name__ == "__main__":
-    """
-    This script combines information from a fieldbook file with an entropy tar file,
-    producing a unified JSON file for indexing a single scan date.
-    To cover all scan dates, execute this script multiple times and changing the entropy_file_path argument.
-    """
     # Check if the correct number of arguments are provided
     if len(sys.argv) != 3:
         print("Usage: python data_preparation.py <fieldbook_csv_path> <entropy_file_path>")
@@ -221,10 +256,8 @@ if __name__ == "__main__":
     if sys.argv[1] == "--help" or sys.argv[1] == "-h":
         print("Usage: python script.py <fieldbook_csv_path> <entropy_file_path>")
         print("  fieldbook_csv_path: Absolute path of the fieldbook CSV file in iRODS")
-        print("  entropy_file_path: Absolute path of the entropy.tar file corresponding to a scan date in iRODS")
+        print("  entropy_file_path: Absolute path of the entropy.tar file corresponding to a " +
+              "scan date in iRODS")
         sys.exit(0)
 
-    fieldbook_csv_path = sys.argv[1]
-    entropy_file_path = sys.argv[2]
-
-    main(fieldbook_csv_path, entropy_file_path)
+    main(fieldbook_csv_path=sys.argv[1], entropy_file_path=sys.argv[2])
